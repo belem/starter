8ad83d63ceb3908d7fed05aa6fdac684c5987847
Belem <belem@users.noreply.github.com>
2024-08-29 13:51:03 +0800

    Initial commit
    init - sveltekit + shadcn-svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="js">
+	import { Icons } from '$lib/components/docs/index';
+	import { toggleMode } from 'mode-watcher';
+	import { Button } from '$lib/components/ui/button/index.js';
+</script>
+
+<Button href="/auth/login" variant="ghost">Login</Button>
+
+<Button on:click={toggleMode} variant="outline" size="icon">
+	<Icons.sun
+		class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+	/>
+	<Icons.moon
+		class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+	/>
+	<span class="sr-only">Toggle theme</span>
+</Button>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
